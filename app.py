fd2d63087b1725cc45d973f7448e81701639f419
Sam Sulaimanov <sam@octanis.ch>
2024-02-21 12:39:12 +0100

    fixing apostrophe bug

--- app.py
+++ app.py
@@ -143,16 +143,16 @@ if leg1_connections:
             col1, col2, col3 = st.columns([3, 0.5, 3])
             with col1:
                 st.caption(f"{vehicle1} {leg1['line']} to {terminal1} {track1}")
-                st.markdown(f'<span class="big-font">{departure1_time}</span><br>{st.session_state['leg1_from']} ',unsafe_allow_html=True)
-                st.markdown(f'<span class="big-font">{arrival1_time}</span><br>{st.session_state['leg1_to']} ',unsafe_allow_html=True)
+                st.markdown(f'<span class="big-font">{departure1_time}</span><br>' + f"{st.session_state['leg1_from']}",unsafe_allow_html=True)
+                st.markdown(f'<span class="big-font">{arrival1_time}</span><br>' + f"{st.session_state['leg1_to']}",unsafe_allow_html=True)
                 
             with col2:
                 st.markdown("→")
                 
             with col3:
                 st.caption(f"{vehicle2} {leg2['line']} to {terminal2} {track2}")
-                st.markdown(f'<span class="big-font">{departure2_time}</span><br>{st.session_state['leg2_from']} ',unsafe_allow_html=True)
-                st.markdown(f'<span class="big-font">{arrival2_time}</span><br>{st.session_state['leg2_to']} ',unsafe_allow_html=True)
+                st.markdown(f'<span class="big-font">{departure2_time}</span><br>'  + f"{st.session_state['leg2_from']}",unsafe_allow_html=True)
+                st.markdown(f'<span class="big-font">{arrival2_time}</span><br>'  + f"{st.session_state['leg2_to']}",unsafe_allow_html=True)
 else:
     st.error("Unable to fetch connections. Please check your inputs and try again.")
 
